13ae2f54b6b48915302f5060089b7b99240a0a85
Francesco Ilario <filario@redhat.com>
2024-03-18 12:56:56 +0100

    Add public-viewer configuration
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ pkg/configuration/publicviewer_config.go
@@ -0,0 +1,25 @@
+package configuration
+
+import (
+	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
+)
+
+type PublicViewerConfig struct {
+	Config toolchainv1alpha1.PublicViewerConfig
+}
+
+func (c PublicViewerConfig) Enabled() bool {
+	return c.Config.Enabled
+}
+
+func (c PublicViewerConfig) Username() *string {
+	return &c.Config.Username
+}
+
+func (c PublicViewerConfig) IsPublicViewer(username string) bool {
+	if !c.Enabled() {
+		return false
+	}
+
+	return c.Config.Username == username
+}
